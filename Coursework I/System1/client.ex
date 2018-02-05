e1b03906d6fce28fcc36ca70e786b83e53bce0fb
ap8415 <ap8415@imperial.ac.uk>
2018-02-05 16:16:40 +0000

    Finished System 1.

--- Coursework I/System1/client.ex
+++ Coursework I/System1/client.ex
@@ -6,25 +6,29 @@ defmodule Client do
     end
   end
 
-  def send_client(neighbours, number_of_broadcasts, peer_id, main_peer, send_state) do
-    send_state = for i <- 0..length(neighbours)-1 do
-      peer = Enum.at(neighbours, i)
-      send peer, {:msg, peer_id}
-      Enum.at(send_state, i) + 1
-    end
-
-    if (number_of_broadcasts > 0) do
-      send_client(neighbours, number_of_broadcasts - 1, peer_id, main_peer, send_state)
-    else
-      send main_peer, {:sends_finished, send_state}
-    end
-  end
-
+  # The broadcast strategy used is to split each client into two threads;
+  # the original one for receiving, and a new one for sending messages.
   defp start_broadcast(neighbours, peer_id) do
     send_state = for _ <- 1..length(neighbours), do: 0
     receive_state = for _ <- 1..length(neighbours), do: 0
     spawn(Client, :send_client, [neighbours, 1000, peer_id, self(), send_state])
     receive_client(peer_id, send_state, receive_state, 5000, false)
+  end
+
+  def send_client(neighbours, number_of_broadcasts, peer_id, main_peer, send_state) do
+    if (number_of_broadcasts == 0) do
+      send main_peer, {:sends_finished, send_state}
+    else
+      send_state = for i <- 0..length(neighbours)-1 do
+        peer = Enum.at(neighbours, i)
+        send peer, {:msg, peer_id}
+        Enum.at(send_state, i) + 1
+      end
+
+      if (number_of_broadcasts > 0) do
+        send_client(neighbours, number_of_broadcasts - 1, peer_id, main_peer, send_state)
+      end
+    end
   end
 
   defp receive_client(peer_id, send_state, receive_state, count, sends_finished) do
